69c088c5c27ce84457f36c5a00eed63d0cf93b3b
antonmolodykh <antonmolodykh96@gmail.com>
2017-11-01 18:45:36 +0600

    add packages

--- start.sh
+++ start.sh
@@ -1,31 +1,30 @@
 #!/bin/bash
+
 export DISPLAY=:0.0
+# usermod -a -G video pi
 
-echo $DISPLAY
-echo "Test"
-echo $HOSTNAME
 
-cp /etc/hosts ~/hosts.new
-sed -i "s/localhost.localdomain/$HOSTNAME/" ~/hosts.new
-cat ~/hosts.new > /etc/hosts
+echo "pre x"
+#rm ~/.Xauthority
+startx /usr/bin/python /home/pi/screenly/viewer.py
+# umount /dev/shm && mount -t tmpfs shm /dev/shm
+# rm /tmp/.X0-lock &>/dev/null || true
 
-umount /dev/shm && mount -t tmpfs shm /dev/shm
-rm /tmp/.X0-lock &>/dev/null || true
+# su - pi -c "unset XAUTHORITY; xauth add $(xauth list)"
+# startx &
 
-su - pi -c "unset XAUTHORITY; xauth add $(xauth list)"
-startx &
+# xset q &>/dev/null
+# xset_is_ready=$?
+# while [ "$xset_is_ready" != "0" ]; do
+# 	xset q &>/dev/null
+# 	xset_is_ready=$?
+#     echo "xset q returned $xset_is_ready. Waiting for 1 s..."
+#     sleep 1
+# done
 
-xset q &>/dev/null
-xset_is_ready=$?
-while [ "$xset_is_ready" != "0" ]; do
-	xset q &>/dev/null
-	xset_is_ready=$?
-    echo "xset q returned $xset_is_ready. Waiting for 1 s..."
-    sleep 1
-done
-export DISPLAY=:0.0   
-su - pi -c "/usr/bin/matchbox-window-manager -use_titlebar no -use_cursor no"
-su - pi -c "python /home/pi/screenly/server.py" &
-su - pi -c "python /home/pi/screenly/viewer.py"
+# su - pi -c "/usr/bin/matchbox-window-manager -use_titlebar no -use_cursor no"
+# su - pi -c "python /home/pi/screenly/websocket_server_layer.py" &
+# su - pi -c "python /home/pi/screenly/server.py" &
+# su - pi -c "python /home/pi/screenly/viewer.py"
 
 
