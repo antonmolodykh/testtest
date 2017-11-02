44eb5d25687ec7d196f9ceb4666029a07885b630
antonmolodykh <antonmolodykh96@gmail.com>
2017-11-02 19:28:19 +0600

    add styles uzbl

--- start.sh
+++ start.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 
+mkdir /data/screenly /data/.screenly /data/screenly_assets /data/.config /data/.config/uzbl
+cp -n ansible/roles/screenly/files/screenly.conf /data/.screenly/screenly.conf
+cp -n ansible/roles/screenly/files/screenly.db /data/.screenly/screenly.db
+cp -n loading.png /data/screenly/loading.png
+cp -n ansible/roles/screenly/files/uzbl-config /data/.config/uzbl/config-screenly
+# cp -n ansible/roles/screenly/files/screenly_utils.sh /usr/local/bin/screenly_utils.sh
+# cp -n ansible/roles/screenly/files/screenly_overrides /tmp/screenly_overrides
+# echo 'pi ALL=NOPASSWD:/usr/local/bin/screenly_utils.sh' >> /etc/X11/xinit/xserverrc
+
+
+chown -R pi:pi /data
+
 export DISPLAY=:0.0
 usermod -a -G video pi
 usermod -a -G tty pi
@@ -11,16 +23,11 @@ sed -i "s/localhost.localdomain/$HOSTNAME/" ~/hosts.new
 cat ~/hosts.new > /etc/hosts
 
 
-echo "pre x"
-echo $DISABLE_UPDATE_CHECK
-
-
 rm /tmp/.X0-lock &>/dev/null || true
 
 su - pi -c "unset XAUTHORITY; xauth add $(xauth list)"
-su - pi -c "python /home/pi/screenly/websocket_server_layer.py" &
-su - pi -c "python /home/pi/screenly/server.py" &
-startx &
+su - pi -c "HOME=/data python /home/pi/screenly/server.py" &
+/usr/bin/X &
 
 xset q &>/dev/null
 xset_is_ready=$?
@@ -30,5 +37,9 @@ while [ "$xset_is_ready" != "0" ]; do
     echo "xset q returned $xset_is_ready. Waiting for 1 s..."
     sleep 1
 done
-#su - pi -c "/usr/bin/matchbox-window-manager -use_titlebar no -use_cursor no"
-su - pi -c "DISABLE_UPDATE_CHECK=True python /home/pi/screenly/viewer.py"
+
+export DISPLAY=:0.0
+su - pi -c "/usr/bin/matchbox-window-manager -use_titlebar no -use_cursor no" &
+# echo "POST WINDOWS MANAGER"
+su - pi -c "DISABLE_UPDATE_CHECK=True HOME=/data python /home/pi/screenly/viewer.py"&
+su - pi -c "HOME=/data python /home/pi/screenly/websocket_server_layer.py"
