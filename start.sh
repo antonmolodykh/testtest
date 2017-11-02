26826c0f9b48ef694889a37c07fc56a9934973d9
antonmolodykh <antonmolodykh96@gmail.com>
2017-11-02 16:14:04 +0600

    some cnanges

--- start.sh
+++ start.sh
@@ -12,30 +12,20 @@ cat ~/hosts.new > /etc/hosts
 echo "pre x"
 
 
-
-# umount /dev/shm && mount -t tmpfs shm /dev/shm
 rm /tmp/.X0-lock &>/dev/null || true
 
 su - pi -c "unset XAUTHORITY; xauth add $(xauth list)"
-
+su - pi -c "python /home/pi/screenly/websocket_server_layer.py" &
 su - pi -c "python /home/pi/screenly/server.py" &
 startx &
-sleep 10
+
+xset q &>/dev/null
+xset_is_ready=$?
+while [ "$xset_is_ready" != "0" ]; do
+  xset q &>/dev/null
+  xset_is_ready=$?
+    echo "xset q returned $xset_is_ready. Waiting for 1 s..."
+    sleep 1
+done
+su - pi -c "/usr/bin/matchbox-window-manager -use_titlebar no -use_cursor no"
 su - pi -c "python /home/pi/screenly/viewer.py"
-# startx &
-
-# xset q &>/dev/null
-# xset_is_ready=$?
-# while [ "$xset_is_ready" != "0" ]; do
-# 	xset q &>/dev/null
-# 	xset_is_ready=$?
-#     echo "xset q returned $xset_is_ready. Waiting for 1 s..."
-#     sleep 1
-# done
-
-# su - pi -c "/usr/bin/matchbox-window-manager -use_titlebar no -use_cursor no"
-# su - pi -c "python /home/pi/screenly/websocket_server_layer.py" &
-# su - pi -c "python /home/pi/screenly/server.py" &
-# su - pi -c "python /home/pi/screenly/viewer.py"
-
-
